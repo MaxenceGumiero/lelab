cb3d1ed112d8624eb88293cb149104937c7c6c08
MaxenceGumiero <47859691+MaxenceGumiero@users.noreply.github.com>
2020-02-20 06:56:38 +0100

    Initial commit

---
+++ lelab/lelab/Views/CountPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lelab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CountPage : ContentPage
+    {
+        int count = 0;
+        public CountPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Increment(object sender, EventArgs e)
+        {
+            count++;
+            counter.Text = count.ToString();
+        }
+    }
+}
---
+++ lelab/lelab/Views/MenuPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lelab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToCounter(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new CountPage());
+        }
+
+        private void TabsButtonClick(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new TabsPage());
+        }
+    }
+}
