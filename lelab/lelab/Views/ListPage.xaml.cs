c6fe40340368f2bb560fd005663ad6a9ef96ea25
MaxenceGumiero <47859691+MaxenceGumiero@users.noreply.github.com>
2020-02-21 06:28:13 +0100

    Adding of the list

--- lelab/lelab/Views/ListPage.xaml.cs
+++ lelab/lelab/Views/ListPage.xaml.cs
@@ -9,12 +9,40 @@ using Xamarin.Forms.Xaml;
 
 namespace lelab.Views
 {
+    public class Article
+    {
+        public string Nom { get; set; }
+        public string Prix { get; set; }
+        public string Description { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListPage : ContentPage
     {
+        List<Article> articles;
         public ListPage()
         {
             InitializeComponent();
+
+            articles = new List<Article>();
+            articles.Add(new Article { Nom = "Lait", Prix = "0.90 €", Description="Pack de lait" });
+            articles.Add(new Article { Nom = "Chocolat", Prix = "2.60 €", Description = "80% de cacao" });
+            articles.Add(new Article { Nom = "Pain", Prix = "1.80 €", Description = "Pain aux céréales" });
+            articles.Add(new Article { Nom = "Beurre", Prix = "1.50 €", Description = "Beurre demi-sel" });
+            articles.Add(new Article { Nom = "Sucre", Prix = "1 €", Description = "Sucre de canne" });
+
+            maListView.ItemsSource = articles;
+
+            maListView.ItemSelected += (sender, e) =>
+            {
+                if (maListView.SelectedItem != null) 
+                {
+                    Article item = maListView.SelectedItem as Article;
+
+                    DisplayAlert(item.Nom, item.Description, "Retour");
+                    maListView.SelectedItem = null;
+                }
+            };
         }
     }
 }
--- lelab/lelab/obj/Debug/netstandard2.0/Views/ListPage.xaml.g.cs
+++ lelab/lelab/obj/Debug/netstandard2.0/Views/ListPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace lelab.Views {
     public partial class ListPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView maListView;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ListPage));
+            maListView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "maListView");
         }
     }
 }
